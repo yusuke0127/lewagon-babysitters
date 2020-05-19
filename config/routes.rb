2605726050a2eb68c05b11ea221c0e74da9c16fc
Pins Thoo <aipeng.thoo@gmail.com>
2020-05-19 11:50:07 +0800

    updated-show-page-from-index

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/babysitters', to: 'babysitters#index', as: :babysitters
+  get '/babysitters/:id', to: 'babysitters#show', as: :babysitter
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
