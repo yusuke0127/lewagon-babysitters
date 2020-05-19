44d595e740004da3b09960238f4948c5b3a189f3
Pins Thoo <aipeng.thoo@gmail.com>
2020-05-19 15:29:01 +0800

    Working reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,7 +3,25 @@ class ReservationsController < ApplicationController
     @reservations = Reservation.all
   end
 
-  def index_babysitter
-    @reservations = Reservation.where(params[:babysitter_id])
+  # def index_babysitter
+  #   @reservations = Reservation.where(babysitter_id: params[:babysitter_id])
+  # end
+
+  def create
+    @babysitter = Babysitter.find(params[:babysitter_id])
+    @user = User.find(@babysitter.user_id)
+    @reservation = Reservation.new(reservation_params)
+    @reservation.babysitter = @babysitter
+    @reservation.user = @user
+    if @reservation.save
+      redirect_to reservations_path
+    else
+      render 'babysitters/index'
+    end
+  end
+
+  private
+  def reservation_params
+    params.require(:reservation).permit(:start_time, :end_time, :babysitter_id)
   end
 end
