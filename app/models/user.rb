919f50cfec45f988aa9ed2b8920919b5648f7ac8
Pins Thoo <aipeng.thoo@gmail.com>
2020-05-21 16:07:26 +0800

    added a lot of things and the search bar is working :)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
- has_one :babysitter
- has_many :babysitters, through: :reservations
- has_one_attached :photo
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
+  has_one :babysitter
+  has_many :babysitters, through: :reservations
+  has_one_attached :photo
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_021122) do
+ActiveRecord::Schema.define(version: 2020_05_21_044722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,8 @@ ActiveRecord::Schema.define(version: 2020_05_20_021122) do
     t.string "last_name"
     t.string "phone_number"
     t.string "image_url"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
