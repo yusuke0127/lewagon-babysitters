04e31fb3a188bc276dd0f25c5c09ee8016be7147
Pins Thoo <aipeng.thoo@gmail.com>
2020-05-22 09:47:45 +0800

    updated calendar without hard refresh

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,14 +24,15 @@ require("channels")
 
 // External imports
 import "bootstrap";
-import "../plugins/flatpickr"
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initAutocomplete } from '../plugins/init_autocomplete';
+import { initFlatpickr } from '../plugins/flatpickr';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initAutocomplete();
+  initFlatpickr();
 });
